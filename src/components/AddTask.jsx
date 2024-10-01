de73ab6126ee385951c50b5d35603813b8db2d0a
Fellipe Reis <reisfellipe11@gmail.com>
2024-10-01 16:08:28 -0300

    connecting the twcss

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,11 @@
+function AddTask() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default AddTask
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,12 @@
+
+function Tasks(){
+
+    return(
+        <div>
+            <h1>Gerenciador de Tarefas</h1>
+
+        </div>
+    )
+
+}
+export default Tasks
